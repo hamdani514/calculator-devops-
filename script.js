f5cfc46ac192df6890ff3838383531be7e42847e
Daniyal Abbas <adanial337@gmail.com>
2026-02-21 16:47:11 +0500

    Added unit testing with Jest

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ function calculate() {
     let display = document.getElementById("display");
     display.value = eval(display.value);
 }
+function add(a, b) {
+    return a + b;
+}
+
+if (typeof module !== "undefined") {
+    module.exports = { add };
+}
